d64ad9238fcd227b63a57ccbb8206ca6e17f4a2b
Asadbek <71205691+AsadbekAbdullayev@users.noreply.github.com>
2025-02-15 22:57:14 +0500

    Update index.tsx

--- src/components/Bubble/index.tsx
+++ src/components/Bubble/index.tsx
@@ -6,6 +6,7 @@ import { animated, useSpring } from "react-spring";
 
 
 
+
 import { useMovementSpring, MovementSquareBorders } from "./useMovementSpring";
 import bubbleModuleCss from "./Bubble.module.css";
 import { useGradientSpring } from "./useGradientSpring";
